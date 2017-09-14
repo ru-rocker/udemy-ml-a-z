d4c91efb9a98a3fe3781c880788896f4d5c77baf
ru.rocker81@gmail.com <ru.rocker81@gmail.com>
2017-09-14 16:31:49 +0700

    data prepocessing

--- Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.R
+++ Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.R
@@ -3,6 +3,7 @@
 # Importing the dataset
 dataset = read.csv('Data.csv')
 
+# Taken care of missing data
 dataset$Age = ifelse(test = is.na(dataset$Age),
                      yes = ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
                      no = dataset$Age
@@ -13,14 +14,25 @@ dataset$Salary = ifelse(test = is.na(dataset$Salary),
                         no = dataset$Salary
                         )
 
+#Encoding categorical data
+dataset$Country = factor(dataset$Country,
+                         levels = c('France','Spain','Germany'),
+                         labels = c(1,2,3)
+                         )
+
+dataset$Purchased = factor(dataset$Purchased,
+                           levels = c('Yes','No'),
+                           labels = c(1,0)
+                           )
+
 # Splitting the dataset into the Training set and Test set
 # install.packages('caTools')
 library(caTools)
 set.seed(123)
-split = sample.split(dataset$DependentVariable, SplitRatio = 0.8)
+split = sample.split(dataset$Purchased, SplitRatio = 0.8)
 training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
 
-# Feature Scaling
-# training_set = scale(training_set)
-# test_set = scale(test_set)
+# Feature Scaling: category is a vector, so it has to be excluded from scale
+training_set[,2:3] = scale(training_set[,2:3])
+test_set[,2:3] = scale(test_set[,2:3])
